47fe2c08d30b7b08e298f57d5161c9117f679e2e
Mr.Tejas Dattatray Kolhe <147149702+tejaskolhe2004@users.noreply.github.com>
2024-02-29 21:29:35 +0530

    prime.java

--- prime.java
+++ prime.java
@@ -1,40 +1,26 @@
 import java.util.Scanner;
-public class prime {
-
+public class Main
+{
     public static void main(String[] args) {
-      // Input number
+     
+    int count=0;
+    System.out.println("enter any number");
+    Scanner sc =new Scanner(System.in);
+     int n= sc.nextInt();
+
+     for(int i=1;i<=n;i++) {
       
-      System.out.println("enter the number");
-      Scanner sc=new Scanner(System.in);
-      int n=sc.nextInt();
-
-
-  
-      // Holds the count of values
-      int count = 0;
-  
-      // There is no prime number less than 2.
-      if (n <= 1) {
-        System.out.println("The number is not prime");
-        return;
-      }
-  
-      // Do a for loop
-      for (int i = 1; i <= n / 2; i++) {
-        if (n % i == 0) {
-          count++;
-        }
-      }
-  
-      // If the number of factors is greater than 1,
-      // the number is not prime.
-      if (count > 1) {
-          System.out.println("The number is not prime");
-      }
-      else {
-        System.out.println("The number is prime");
-      }
+     if (n%i==0) {
+      count++;
+    
+     }
     }
+           if(count==2)
+           System.out.println(" prime");
+           else
+           System.out.println("not prime");
+
+     }
+            
   }
-  
-  
+
